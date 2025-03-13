5d0405bfa3c77fdbe2e3e1d8892be20b21371479
Ohne-Dich <lawerenz.f@gmail.com>
2025-03-13 17:06:25 +0100

    Recovered project files after repository repair

---
+++ main.py
@@ -0,0 +1,57 @@
+import sys
+import pygame
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
+
+
+    asteroid_field = AsteroidField()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collisions(player) == True:
+                print("Game Over!")
+                sys.exit()
+        
+            for bullet in shots:
+              if asteroid.collisions(bullet):
+                  asteroid.split()
+                  bullet.kill()
+
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
+        pygame.display.flip()
+
+        # fps limit 60:
+        dt = clock.tick(60) / 1000
+
+if __name__ == "__main__":
+    main()
